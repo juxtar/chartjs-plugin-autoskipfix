9363d89b75b7dffc16d495aed252963bf26a1f80
Julian Mazzei <julian.mazzei@ppst.com>
2020-01-21 09:12:09 -0300

    Implement autoskip function

--- dist/chartjs-plugin-autoskipfix.js
+++ dist/chartjs-plugin-autoskipfix.js
@@ -6,10 +6,35 @@
 
     Chart = Chart && Chart.hasOwnProperty('default') ? Chart['default'] : Chart;
 
+    function skip(ticks, spacing) {
+      var end = ticks.length;
+      var count = 0;
+      var next = 0;
+      spacing = Math.ceil(spacing);
+      return ticks.map(function (tick, i) {
+        if (i === next || i === end - 1) {
+          tick._index = i;
+          count++;
+          next = Math.round(count * spacing);
+        } else {
+          delete tick.label;
+        }
+
+        return tick;
+      });
+    }
+
     function AutoSkipFixPlugin(Chart) {
       Chart.Scale.prototype._autoSkip = function (ticks) {
-        // TODO: Implement new autoskip function
-        return ticks;
+        var me = this;
+        var tickOpts = me.options.ticks;
+        var axisLength = me._length;
+        var ticksLimit = tickOpts.maxTicksLimit || axisLength / me._tickSize() + 1;
+        var spacing = Math.max((ticks.length - 1) / ticksLimit, 1);
+        return skip(ticks, spacing).reduce(function (list, tick) {
+          if (tick._index != null) list.push(tick);
+          return list;
+        }, []);
       };
 
       return {
--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,36 @@
+function skip(ticks, spacing) {
+	var end = ticks.length;
+	var count = 0;
+	var next = 0;
+
+	spacing = Math.ceil(spacing);
+
+    return ticks.map((tick, i) => {
+        if (i === next || i === end - 1) {
+			tick._index = i;
+			count++;
+			next = Math.round(count * spacing);
+		} else {
+			delete tick.label;
+        }
+        return tick;
+    });
+}
+
 export function AutoSkipFixPlugin(Chart) {
     Chart.Scale.prototype._autoSkip = function(ticks) {
-        // TODO: Implement new autoskip function
+        var me = this;
+        var tickOpts = me.options.ticks;
+        var axisLength = me._length;
+        var ticksLimit = tickOpts.maxTicksLimit || axisLength / me._tickSize() + 1;
+        var spacing = Math.max((ticks.length - 1) / ticksLimit, 1);
 
-        return ticks;
+        return skip(ticks, spacing)
+                .reduce((list, tick) => {
+                    if (tick._index != null)
+                        list.push(tick);
+                    return list;
+                }, []);
     };
 
     return {
